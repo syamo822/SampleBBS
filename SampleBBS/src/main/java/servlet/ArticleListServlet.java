f11d3454822c9ea867330e79a5004a5003a389bc
syamo822 <tc2y22336@stu.tamagawa.ac.jp>
2024-06-19 18:49:13 +0900

    Art編集

--- SampleBBS/src/main/java/servlet/ArticleListServlet.java
+++ SampleBBS/src/main/java/servlet/ArticleListServlet.java
@@ -23,6 +23,7 @@ public class ArticleListServlet extends HttpServlet {
         super();
     }
 
+    //Article編集
     //POSTアクセスされた場合は，doGetに丸投げ
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request,response);
